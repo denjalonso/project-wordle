e7443b2bce788114a6ca85c0a63907b035d06c07
Denis Juan Alonso Glez <denjalonso@gmail.com>
2024-06-21 17:34:52 +0100

    Banner refactor

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,7 +4,7 @@ import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessForm from "../GuessForm";
 import GuessResults from "../GuessesList";
-import ResultBanner from "../ResultBanner";
+import Banner, { HappyBanner, SadBanner } from "../ResultBanner";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
@@ -32,7 +32,13 @@ function Game() {
     <>
       <GuessResults guesses={guesses} answer={answer} />
       <GuessForm onGuess={handleOnGuess} isDisabled={status !== "running"} />
-      <ResultBanner status={status} intents={intents} answer={answer} />
+      <Banner status={status}>
+        {status === "won" ? (
+          <HappyBanner intents={intents} />
+        ) : (
+          <SadBanner answer={answer} />
+        )}
+      </Banner>
     </>
   );
 }
